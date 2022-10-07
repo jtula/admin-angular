46f575d43b91141609d287a56d679babdaace5f9
Jose Antonio Tula Leyva <joseantonio@jdmlabs.io>
2022-10-07 09:33:09 -0400

    feat: add layout skeleton

--- libs/dashboard/ui/src/lib/dashboard-ui.module.ts
+++ libs/dashboard/ui/src/lib/dashboard-ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AppLayoutComponent],
 })
 export class DashboardUiModule {}
